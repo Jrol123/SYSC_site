49c0b27546bb255755016abc35af8ee72049dc8e
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-11-08 18:25:23 +1000

    Update nav-bar.js

--- Frontend/js/nav-bar.js
+++ Frontend/js/nav-bar.js
@@ -8,7 +8,7 @@ hamburger.addEventListener('click', ()=>{
    //Animate Links
     navLinks.classList.toggle("open");
     links.forEach(link => {
-        link.classList.toggle("fade");
+        //link.classList.toggle("fade");
     });
 
     //Hamburger Animation
