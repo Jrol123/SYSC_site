60698afeec37b90479c37e98ef27552f5022b441
Tyferse <qwerty482803@gmail.com>
2023-11-08 14:02:13 +1000

    Default django project created

---
+++ smu_site/smu_site/apps/SHC/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ShcConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'SHC'
+    verbose_name = 'ГЖС'
---
+++ smu_site/smu_site/apps/representatives/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class RepresentativesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'representatives'
+    verbose_name = 'Представители университетов'
