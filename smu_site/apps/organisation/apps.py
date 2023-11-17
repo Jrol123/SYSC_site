fdef86ab20de196518cb125019515c6c805b1702
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-11-17 18:13:43 +1000

    Create apps.py

---
+++ smu_site/apps/organisation/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class RepresentativesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'organisation'
+    verbose_name = 'Об Организаций'
