925a31aeaf448cba021c71f6c7104ceca716a552
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-11-18 21:48:26 +1000

    Delete apps.py

--- smu_site/apps/organisation/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class RepresentativesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'organisation'
-    verbose_name = 'Об Организаций'
