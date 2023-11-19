29ae080da6e12086146aa96a7c3cf7d71c7eb9e2
Tyferse <qwerty482803@gmail.com>
2023-11-19 19:07:57 +1000

    Added Image model in news app.

--- smu_site/settings.py
+++ smu_site/settings.py
@@ -133,7 +133,13 @@ STATICFILES_DIRS = [
    os.path.join(PROJECT_ROOT, "static"),
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
