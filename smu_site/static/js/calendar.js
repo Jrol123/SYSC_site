a6f4d09c9282d97e31426c44576fa898e42fbd87
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-11-27 22:22:57 +1000

    Update calendar.js

--- smu_site/static/js/calendar.js
+++ smu_site/static/js/calendar.js
@@ -1,78 +1,78 @@
  
-                            const currentDate = new Date();
-                            let currentMonth = currentDate.getMonth();
-                            let currentYear = currentDate.getFullYear();
+const currentDate = new Date();
+let currentMonth = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
 
-                            const monthNames = [
-                            "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
-                            ];
+const monthNames = [
+  "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+];
 
-                            function generateCalendar() {
-                              const calendarGrid = document.querySelector(".calendar-grid");
-                              calendarGrid.innerHTML = "";
+function generateCalendar() {
+  const calendarGrid = document.querySelector(".calendar-grid");
+  calendarGrid.innerHTML = "";
                               
-                              // Create header for the current month
-                              const currentMonthHeader = document.querySelector(".current-month");
-                              currentMonthHeader.textContent = monthNames[currentMonth] + " " + currentYear;
+  // Create header for the current month
+  const currentMonthHeader = document.querySelector(".current-month");
+  currentMonthHeader.textContent = monthNames[currentMonth] + " " + currentYear;
                               
-                              // Get the first day of the month
-                              const firstDay = new Date(currentYear, currentMonth, 1).getDay();
+// Get the first day of the month
+  const firstDay = new Date(currentYear, currentMonth, 1).getDay();
                               
-                              // Get the number of days in the month
-                              const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+// Get the number of days in the month
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
                               
-                              // Generate calendar days
-                              for (let i = 0; i < firstDay; i++) {
-                                const emptyDay = document.createElement("div");
-                                emptyDay.classList.add("calendar-day");
-                                calendarGrid.appendChild(emptyDay);
-                              }
+// Generate calendar days
+  for (let i = 0; i < firstDay; i++) {
+    const emptyDay = document.createElement("div");
+    emptyDay.classList.add("calendar-day");
+    calendarGrid.appendChild(emptyDay);
+  }
                               
-                              for (let i = 1; i <= daysInMonth; i++) {
-                                const calendarDay = document.createElement("div");
-                                calendarDay.classList.add("calendar-day");
-                                calendarDay.textContent = i;
+  for (let i = 1; i <= daysInMonth; i++) {
+    const calendarDay = document.createElement("div");
+    calendarDay.classList.add("calendar-day");
+    calendarDay.textContent = i;
                                 
-                                if (currentDate.getMonth() === currentMonth && currentDate.getFullYear() === currentYear && currentDate.getDate() === i) {
-                                  calendarDay.classList.add("today");
-                                }
+    if (currentDate.getMonth() === currentMonth && currentDate.getFullYear() === currentYear && currentDate.getDate() === i) {
+      calendarDay.classList.add("today");
+    }
                                 
-                                calendarDay.addEventListener("click", selectDate);
+    calendarDay.addEventListener("click", selectDate);
                                 
-                                calendarGrid.appendChild(calendarDay);
-                              }
-                            }
+    calendarGrid.appendChild(calendarDay);
+  }
+}
 
-                            function selectDate(event) {
-                              const selectedDay = document.querySelector(".selected");
+function selectDate(event) {
+  const selectedDay = document.querySelector(".selected");
                               
-                              if (selectedDay) {
-                                selectedDay.classList.remove("selected");
-                            }
+  if (selectedDay) {
+    selectedDay.classList.remove("selected");
+  }
 
-                            event.target.classList.add("selected");
-                            }
+  event.target.classList.add("selected");
+}
 
-                            function prevMonth() {
-                            if (currentMonth === 0) {
-                            currentMonth = 11;
-                            currentYear--;
-                            } else {
-                            currentMonth--;
-                            }
+function prevMonth() {
+  if (currentMonth === 0) {
+    currentMonth = 11;
+    currentYear--;
+  } else {
+    currentMonth--;
+  }
 
-                            generateCalendar();
-                            }
+  generateCalendar();
+}
 
-                            function nextMonth() {
-                            if (currentMonth === 11) {
-                            currentMonth = 0;
-                            currentYear++;
-                            } else {
-                            currentMonth++;
-                            }
+function nextMonth() {
+  if (currentMonth === 11) {
+    currentMonth = 0;
+    currentYear++;
+  } else {
+    currentMonth++;
+}
 
-                            generateCalendar();
-                            }
+  generateCalendar();
+}
 
-                            generateCalendar();
+  generateCalendar();
