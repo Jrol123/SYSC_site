c73b8b86c9d45c051ccfc10105ea04221201f369
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-11-28 20:08:29 +1000

    Create todays_date.js

---
+++ smu_site/static/js/todays_date.js
@@ -0,0 +1,16 @@
+const dateInput = document.getElementById('date');
+dateInput.value = formatDate();
+
+console.log(formatDate());
+
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+
+function formatDate(date = new Date()) {
+  return [
+    date.getFullYear(),
+    padTo2Digits(date.getMonth() + 1),
+    padTo2Digits(date.getDate()),
+  ].join('-');
+}
