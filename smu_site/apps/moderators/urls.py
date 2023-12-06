89715876298002fd47c886966a09cf79d5d0e5e3
GaalN <123535267+GaalN@users.noreply.github.com>
2023-12-06 22:30:14 +0300

    urls.py

--- smu_site/apps/moderators/urls.py
+++ smu_site/apps/moderators/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('account/create_user', views.create_new_user, name='create_user'),
-    path('account', views.profile, name='profile')
+    path('account', views.profile, name='profile'),
+    path('account/news', views.news, name='news'),
+    path('account/moder_guests', views.moder_guests, name='moder_guests'),
+    path('account/grants', views.grants, name='grants'),
+    path('account/gzs', views.gzs, name='gzs'),
+    path('account/add_new_guests', views.add_new_guests, name='add_new_guests')
 ]
