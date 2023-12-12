daf858292fec33defc35b0ca12c42d75db76b23e
Tyferse <qwerty482803@gmail.com>
2023-12-12 19:16:10 +1000

    Added link table between Institutes and represenatives

--- smu_site/apps/representatives/models.py
+++ smu_site/apps/representatives/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ReprInst(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             verbose_name="Представитель")
+    institute = models.ForeignKey("info.Institute",
+                                  on_delete=models.DO_NOTHING,
+                                  verbose_name="Представитель")
