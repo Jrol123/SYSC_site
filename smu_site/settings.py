9b691eda6e09c5f27b4578b802f66c6660f8ae4c
Artemii <angap4@gmail.com>
2023-12-15 00:36:38 +1000

    Тестирование mySQL
    НЕ КОПИРОВАТЬ НА ЛОКАЛЬНУЮ МАШИНУ!

--- smu_site/settings.py
+++ smu_site/settings.py
@@ -88,8 +88,8 @@ WSGI_APPLICATION = 'smu_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # потом переименовать
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS':{"read_default_file": "D:/utilities/ospanel/userdata/MySQL-8.0-Win10/auto.cnf"}
     }
 }
 
