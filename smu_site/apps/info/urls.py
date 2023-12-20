3ef619ee5b1381cfedc4bc85d08c5ac4c4ce2fda
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-12-20 15:14:10 +1000

    Update urls.py

--- smu_site/apps/info/urls.py
+++ smu_site/apps/info/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('organization/', views.organization, name='organization'),
     path('institutes/', views.institutes, name='institutes'),
     path('grant/', views.grant, name='grant'),
+    path('newspage/', views.newspage, name='newspage'),
     path('institutes/<int:inst_id>/', views.institute_info,
          name='institute_info')
 ]
