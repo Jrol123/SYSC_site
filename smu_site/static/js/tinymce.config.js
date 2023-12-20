2c3f5bd6f233439f972509f469def06a6d8f3cb1
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-12-20 23:10:32 +1000

    Create tinymce.config.js

---
+++ smu_site/static/js/tinymce.config.js
@@ -0,0 +1,66 @@
+tinymce.init({
+    selector: '#myTextarea',
+    
+    plugins: [
+    'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
+    'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'code', 'fullscreen', 'insertdatetime',
+    'media', 'table', 'emoticons', 'template'
+    ],
+    toolbar: 'undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | ' +
+    'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
+    'forecolor backcolor emoticons | help | googlemap',
+
+    setup: function (editor) {
+      editor.ui.registry.addButton('googlemap', {
+        text: 'Insert Map',
+        onAction: function () {
+          let link = prompt("Enter the link here.");
+          
+          if(link != ""){
+            const mapCode = '<iframe src="' + link + '" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>';
+            editor.insertContent(mapCode);
+          }
+        },
+      });
+    },
+   
+    init_instance_callback : function(editor) {
+        var freeTiny = document.querySelector('.tox .tox-notification--in');
+       freeTiny.style.display = 'none';
+      },
+    menu: {
+    favs: { title: 'My Favorites', items: 'code visualaid | searchreplace | emoticons' }
+    },
+    menubar: 'favs file edit view insert format tools table help',
+   
+
+    image_title: true,
+    // enable automatic uploads of images represented by blob or data URIs
+    automatic_uploads: true,
+    // add custom filepicker only to Image dialog
+    file_picker_types: 'image',
+    file_picker_callback: function(cb, value, meta) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+
+        input.onchange = function() {
+            var file = this.files[0];
+            var reader = new FileReader();
+
+            reader.onload = function () {
+                var id = 'blobid' + (new Date()).getTime();
+                var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                var base64 = reader.result.split(',')[1];
+                var blobInfo = blobCache.create(id, file, base64);
+                blobCache.add(blobInfo);
+
+                // call the callback and populate the Title field with the file name
+                cb(blobInfo.blobUri(), { title: file.name });
+            };
+            reader.readAsDataURL(file);
+        };
+
+        input.click();
+    }
+});
