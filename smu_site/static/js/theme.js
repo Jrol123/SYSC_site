36e8cebdfbb12da6cafb080b88d6b49b75cb24eb
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-12-22 08:09:22 +1000

    Create theme.js

---
+++ smu_site/static/js/theme.js
@@ -0,0 +1,34 @@
+var mode = localStorage.getItem("mode");
+      $('html').attr(mode, '');
+
+$(document).ready(function(){
+  
+   
+    jQuery('img').each(function(){
+    jQuery(this).attr('src',jQuery(this).attr('src')+ '?' + (new Date()).getTime());
+    });
+ 
+    
+
+    $( "#dark-mode-toggless" ).on( "click", function() {
+      localStorage.setItem("mode", "data-dark-mode");
+      var mode = localStorage.getItem("mode");
+      $('html').attr(mode, '');
+      $('html').removeAttr("light-dark-mode");
+
+
+    });
+
+    $( "#light-mode-toggless" ).on( "click", function() {
+      localStorage.setItem("mode", "light-dark-mode");
+      var mode = localStorage.getItem("mode");
+      $('html').attr(mode, '');
+      $('html').removeAttr("data-dark-mode");
+    
+    });
+
+  });
+
+    
+
+  
